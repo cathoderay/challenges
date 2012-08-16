a6848f9cf0b53412af8d76067f0f4c12656ee5f0
Ronald Kaiser <raios.catodicos@gmail.com>
2012-08-16 19:17:45 -0300

    level 3 done =D

---
+++ americanas/challenge4.py
@@ -0,0 +1,16 @@
+from challenge import get_price
+ 
+
+def get_yes_link(r):
+    dir(r)
+    from lxml import html
+    import requests
+    xpath = "//a[contains(text(), 'Sim')]/@href"
+    subpath = html.fromstring(r.text).xpath(xpath)[0]
+    url = "http://hughes.sieve.com.br:9090%s" % subpath
+    return requests.get(url).content
+
+
+url = "http://hughes.sieve.com.br:9090/level3/"
+xpath = "//p/text()"
+print get_price(url, xpath, after_fetch=get_yes_link)
