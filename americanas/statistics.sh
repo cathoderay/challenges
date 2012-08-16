fea9a0937116333b7a3d6bafab77034396a51168
Ronald Kaiser <raios.catodicos@gmail.com>
2012-08-16 12:09:54 -0300

    nice percentage, =P

--- americanas/statistics.sh
+++ americanas/statistics.sh
@@ -2,7 +2,7 @@
 
 NUMBER_OF_REQUESTS=`cat batch.out | grep 'Getting price' | wc -l`
 NUMBER_OF_PRICES=`cat batch.out | grep -ER '[0-9]+\.[0-9]+$' | wc -l`
-NUMBER_OF_SUCCESSES=`perl -e "print $NUMBER_OF_PRICES/$NUMBER_OF_REQUESTS"`
+NUMBER_OF_SUCCESSES=`perl -e "print 100*$NUMBER_OF_PRICES/$NUMBER_OF_REQUESTS"`
 
 echo "Number of requests: $NUMBER_OF_REQUESTS"
 echo "Number of prices got: $NUMBER_OF_PRICES"
