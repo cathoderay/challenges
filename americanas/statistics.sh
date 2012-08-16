623fb43104ac3d3b44b3303a85d5d9d3ab0b55bb
Ronald Kaiser <raios.catodicos@gmail.com>
2012-08-16 12:16:32 -0300

    counting successes properly

--- americanas/statistics.sh
+++ americanas/statistics.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#TODO: add unavailables as successes
-
 NUMBER_OF_REQUESTS=`cat batch.out | grep 'Getting price' | wc -l`
 NUMBER_OF_PRICES=`cat batch.out | grep -ER '[0-9]+\.[0-9]+$' | wc -l`
-NUMBER_OF_SUCCESSES=`perl -e "print 100*$NUMBER_OF_PRICES/$NUMBER_OF_REQUESTS"`
+NUMBER_OF_UNAVAILABLES=`cat batch.out | grep 'Unavailable' | wc -l`
+NUMBER_OF_SUCCESSES=`perl -e "print 100*(($NUMBER_OF_PRICES+$NUMBER_OF_UNAVAILABLES)/$NUMBER_OF_REQUESTS)"`
 
 echo "Number of requests: $NUMBER_OF_REQUESTS"
 echo "Number of prices got: $NUMBER_OF_PRICES"
